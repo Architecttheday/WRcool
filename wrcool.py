a91569ef4e633b27670f6d78a89e0eafa3e6b760
Architecttheday <aussi-surfer@gmx.de>
2020-08-30 21:25:29 +0200

    recalculation of switchpoints and time in log

--- wrcool.py
+++ wrcool.py
@@ -1,33 +1,58 @@
 #!/usr/bin/python3
+import time
 from influxdb import InfluxDBClient
 client = InfluxDBClient(host='solaranzeige', port=8086)
 client.switch_database('solaranzeige')
-tempschwelle = 38.5
+tempschwelle = 39.5
 leistungsschwelle = 800
-tempcount = 0
-leistungscount = 0
-rs = client.query('SELECT last("Temperatur") from Service')
-# print ("Result: {0}".format(rs))
-rs2 = client.query('SELECT last("Leistung") from AC')
+#tempcount = 0
+#leistungscount = 0
+luefterstatus = "off"
 
-points = rs.get_points()
-for item in points:
-    print (item['last'])
+while True:
+    try:
+        rs = client.query('SELECT last("Temperatur") from Service')
+        rs2 = client.query('SELECT last("Leistung") from AC')
+        tempcount = 0
+        leistungscount = 0
+        lt = time.localtime()
+        points = rs.get_points()
+        for item in points:
+            print (item['last'])
 
-points = rs2.get_points()
-for item2 in points:
-    print (item2['last'])
+        points = rs2.get_points()
+        for item2 in points:
+            print (item2['last'])
 
-if (item['last']) > tempschwelle:
-    print ('Temperatur > ', tempschwelle)
-    tempcount = 1
+        if (item['last']) > tempschwelle:
+            print ('Temperatur > ', tempschwelle)
+            tempcount = 1
 
-if (item2['last']) > leistungsschwelle:
-    print ('Leistung > ', leistungsschwelle)
-    leistungscount = 1
+        if (item2['last']) > leistungsschwelle:
+            print ('Leistung > ', leistungsschwelle)
+            leistungscount = 1
 
-if tempcount == 1 and leistungscount == 1:
-    print ('Schalte Lüfter ein!')
+        if tempcount == 1 and leistungscount == 1 and luefterstatus == "off":
+            print ('Schalte Lüfter ein!')
+            luefterstatus = "on"
+
+        if tempcount == 0 and leistungscount == 0 and luefterstatus == "on":
+            print ('Schalte Lüfter aus!')
+            luefterstatus = "off"
+
+        fo = open("Schaltzustaende.txt", "a+")
+        text2write = (time.strftime("%Y-%m-%d_%H:%M:%S" , lt) + ";" + str(tempschwelle) + ";" + str(item['last']) + ";" + str(tempcount) + ";" + str(leistungsschwelle) + ";" + str(item2['last']) + ";" + str(leistungscount) + ";" + luefterstatus + "\n")
+        #text2write = (str(tempschwelle) + ";" + str(item['last']) + ";" +  str(tempcount))
+        print (text2write)
+        fo.write( text2write )
+        fo.close()
+        time.sleep(60)
+    except StopIteration:
+      sys.exit()
+
+# Idee:
+# wenn beide Werte = 1 dann einschalten
+# Wenn beide Werte = 0 und eingeschaltet dann ausschalten
 
 
 #temp = rs.items()
