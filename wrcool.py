d5fcb055622ef7236562a0d49d0dd49d031312ad
Architecttheday <aussi-surfer@gmx.de>
2020-08-31 22:05:10 +0200

    implemented mqtt switching and fqhn

--- wrcool.py
+++ wrcool.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import time
 from influxdb import InfluxDBClient
-client = InfluxDBClient(host='solaranzeige', port=8086)
+client = InfluxDBClient(host='solaranzeige.fritz.box', port=8086)
 client.switch_database('solaranzeige')
 tempschwelle = 39.5
 leistungsschwelle = 800
 #tempcount = 0
 #leistungscount = 0
 luefterstatus = "off"
+import paho.mqtt.client as paho
+broker = "solaranzeige.fritz.box"
+mqclient = paho.Client("client-001")
 
 while True:
     try:
@@ -35,10 +38,18 @@ while True:
         if tempcount == 1 and leistungscount == 1 and luefterstatus == "off":
             print ('Schalte Lüfter ein!')
             luefterstatus = "on"
+            mqclient.connect(broker, 1883, 60)
+            mqclient.publish("rfbridge/relay/4/set","1")#publish
+            time.sleep(4)
+            mqclient.disconnect() #disconnect
 
         if tempcount == 0 and leistungscount == 0 and luefterstatus == "on":
             print ('Schalte Lüfter aus!')
             luefterstatus = "off"
+            mqclient.connect(broker, 1883, 60)
+            mqclient.publish("rfbridge/relay/4/set","0")#publish
+            time.sleep(4)
+            mqclient.disconnect() #disconnect
 
         fo = open("Schaltzustaende.txt", "a+")
         text2write = (time.strftime("%Y-%m-%d_%H:%M:%S" , lt) + ";" + str(tempschwelle) + ";" + str(item['last']) + ";" + str(tempcount) + ";" + str(leistungsschwelle) + ";" + str(item2['last']) + ";" + str(leistungscount) + ";" + luefterstatus + "\n")
